53b62e090ea0b4f2dac23db42fe4c0f423d8180c
Toshiaki Maki <makingx@gmail.com>
2015-06-18 19:24:20 +0900

    up

--- src/test/java/demo/DemoApplicationTests.java
+++ src/test/java/demo/DemoApplicationTests.java
@@ -1,30 +1,51 @@
 package demo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.mock.http.MockHttpInputMessage;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 import javax.xml.bind.UnmarshalException;
+import javax.xml.transform.Source;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamSource;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = DemoApplication.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = DemoApplication.class)
 public class DemoApplicationTests {
 
-    @Autowired
-    @Qualifier("httpMessageConverter")
-    HttpMessageConverter<Object> converter;
+    //    @Autowired
+    //    @Qualifier("httpMessageConverter")
+    HttpMessageConverter<Object> converter = new Jaxb2RootElementHttpMessageConverter() {
+        @Override
+        protected Source processSource(Source source) {
+            if (source instanceof StreamSource) {
+                StreamSource streamSource = (StreamSource) source;
+                InputSource inputSource = new InputSource(streamSource.getInputStream());
+                try {
+                    XMLReader ex = XMLReaderFactory.createXMLReader();
+                    ex.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+                    return new SAXSource(ex, inputSource);
+                } catch (SAXException var6) {
+                    this.logger.warn("Processing of external entities could not be disabled", var6);
+                    return source;
+                }
+            } else {
+                return source;
+            }
+        }
+    };
 
     @Test
     public void contextLoads() throws Exception {
